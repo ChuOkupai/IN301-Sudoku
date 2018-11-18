7b759acab7cca81d0986933f48ea96d51a6d40ed
Adrien <adrienadrien@neuf.fr>
2018-11-18 21:13:15 +0100

    rm

--- SOURSOU_Adrien/gestion_sudoku.c
+++
@@ -1,2 +0,0 @@
-#include <stdlib.h>
-#include "gestion_sudoku.h"
--- SOURSOU_Adrien/lire_ecrire.h
+++
@@ -1,8 +0,0 @@
-
-#ifndef __LIRE_ECRIRE_H
-#define __LIRE_ECRIRE_H
-// Lecture d'un fichier .sudoku et stockage dans une variable de type SUDOKU qui est renvoyée
-SUDOKU lire_fichier (char *nom);
-// Ecriture d'un sudoku dans un fichier
-void ecrire_fichier(SUDOKU S);
-#endif
